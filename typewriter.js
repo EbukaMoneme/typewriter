a3bf0b7dcd9c5ae81d11310875c8101e51c7573f
EbukaMoneme <camoneme@gmail.com>
2021-09-21 13:12:28 +0000

    Used setTimeout to create a typewriter animation

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = 'hello there from lighthouse labs';
+
+const typewriter = function(sentence, time) {
+  for (const char in sentence) {
+    setTimeout(() => {
+      if (char == sentence.length - 1) {
+        process.stdout.write(sentence[char] + '\n');
+      }
+      process.stdout.write(sentence[char]);
+    }, time)
+    time += 50;
+  }
+}
+
+typewriter(sentence, 0)
